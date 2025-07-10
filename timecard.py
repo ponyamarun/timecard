3fe54c5add93a50f18cb428652336f53e2f1ecb7
ponyamarun <xnngn.ks@gmail.com>
2025-07-10 16:18:26 +0900

    del json.loads()

--- timecard.py
+++ timecard.py
@@ -11,7 +11,7 @@ SHEET_KEY = st.secrets["sheet_key"]
 #認証
 SCOPE = ["https://spreadsheets.google.com/feeds",
          "https://www.googleapis.com/auth/drive"]
-service_account_info = json.loads(st.secrets["gcp_service_account"])
+service_account_info = st.secrets["gcp_service_account"]
 creds = Credentials.from_service_account_info(service_account_info)
 gc = gspread.authorize(creds)
 
